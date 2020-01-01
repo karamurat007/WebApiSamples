832262e1656d5d26e83dc987b5d76eb09fe745f5
Ayhan Bahadir Meric <bahadirmeric@outlook.com>
2020-01-01 09:26:29 +0300

    ProductsController sayfası eklendi ve isteğe cevap vermesi içi ayarlandı.

---
+++ WebApiTest2/WebApiTest2/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTest2.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController:Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "ProductsController çalıştı.";
+        }
+    }
+}
